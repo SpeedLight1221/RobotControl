5ac8bb4598a53350f6b68a9add5650126ea32fe5
SpeedLight1221 <vojtacharouz@gmail.com>
2024-11-27 17:02:03 +0100

    Add project files.

---
+++ RobotControl/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace RobotControl
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ RobotControl/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace RobotControl
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
